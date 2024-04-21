0e5067c936bbd662a71e1dbfcef3980cea7db42e
bbasa1 <bbasa@phare.normalesup.org>
2024-04-21 19:49:41 +0200

    Preparation et importation des dt et du REI

---
+++ 02_Importations_preparation.R
@@ -0,0 +1,59 @@
+################################################################################
+########### LES FONCTIONS POUR PREPARER LES DT EXPLOITABLES ####################
+################################################################################
+
+
+Faire_dt_SOUS_REI <- function(liste_cols_REI_loc){
+  # Le fichier REI est trop volumineux, pour travailler dessus on va ne sélectionner que quelques colonnes
+  # Cette fonction ne renvoie rien, mais sauvegarde REI_2021_SELECT
+  
+  REI_21 <- data.table(read.xlsx(paste(repo_data, "REI_2021.xlsx", sep = "/")))
+  SOUS_REI <- REI_21[, ..liste_cols_REI]
+  rm(REI_21)
+  save(SOUS_REI, file = paste(repo_bases_intermediaires, "REI_2021_SELECT.RData", sep = "/"))
+}
+
+Importer_et_merge_DT_REI <- function(liste_cols_REI_loc){
+  # Importe et merge : carac_men, carac_tf et SOUS_REI. On ne garde que quelques colonnes de SOUS_REI
+  # Retourne le dt merged
+  # ATTENTION SI ON AJOUTE DES COLONNES IL VA FALLOIR SANS DOUTE AJOUTER DES SETNAMES
+  
+  # Importer le REI et sélectionner les colonnes
+  load(paste(repo_bases_intermediaires, "REI_2021_SELECT.RData", sep = "/"))
+  SOUS_REI <- SOUS_REI[,..liste_cols_REI_loc]
+  
+  # Faire quelques setnames pour éviter les problèmes
+  try(setnames(SOUS_REI, "Numéro.national.du.groupement", "Numéro_national_du_groupement"), silent = TRUE)
+  try(setnames(SOUS_REI, "FB.-.COMMUNE./.TAUX.NET", "FB_COMMUNE_TAUX.NET"), silent = TRUE)
+  try(setnames(SOUS_REI, "FB.-.GFP./.TAUX.APPLICABLE.SUR.LE.TERRITOIRE.DE.LA.COMMUNE", "FN_GFP_TAUX_APPLICABLE_SUR_LE_TERRITOIRE_DE_LA_COMMUNE"), silent = TRUE)
+  try(setnames(SOUS_REI, "FB.-.TSE./.TAUX.NET", "FB_TSE_TAUX_NET"), silent = TRUE)
+  
+  
+  # Importer carac_men et carac_tf
+  carac_men <- data.table(readRDS(paste(repo_data, "carac_men.rds", sep = "/")))
+  carac_tf <- data.table(readRDS(paste(repo_data, "carac_tf.rds", sep = "/")))
+  
+  # Merge TF et MEN
+  dt_merged <- merge(carac_tf, carac_men, by.x = "ident21", by.y = "ident")
+  
+  # Faire attention au typage
+  dt_merged$ccocom_REI <- as.factor(dt_merged$ccocom_REI)
+  dt_merged$ccodep <- as.factor(dt_merged$ccodep)
+  SOUS_REI$DEPARTEMENT <- as.factor(SOUS_REI$DEPARTEMENT)
+  SOUS_REI$COMMUNE <- as.factor(SOUS_REI$COMMUNE)
+  
+  # ATTENTION pour Lyon, Paris, Marseille il faut changer le ccocom pour mettre le code de la commune, et non pas des arrondissements
+  liste_num_Lyon <- 381:389
+  liste_num_Marseille <- 201:216
+  liste_num_Paris <- 101:120
+  
+  dt_merged[, ccocom_REI := ccocom]
+  dt_merged[ccodep == "69" & ccocom_REI %in% liste_num_Lyon, ccocom_REI := "123"] # On met le code de la commune pour avoir le REU, et pas le code des arrondissement
+  dt_merged[ccodep == "13" & ccocom_REI %in% liste_num_Marseille, ccocom_REI := "055"] 
+  dt_merged[ccodep == "75" & ccocom_REI %in% liste_num_Paris, ccocom_REI := "056"]
+  
+  # Puis Merge merged et REI
+  dt_merged_REI_loc <- merge(dt_merged, SOUS_REI, by.x = c("ccodep", "ccocom_REI"), by.y = c("DEPARTEMENT", "COMMUNE"), all.x = TRUE)
+  
+  return(dt_merged_REI_loc)
+}
--- MAIN_SCRIPT.R
+++ MAIN_SCRIPT.R
@@ -17,21 +17,17 @@ repo_bases_intermediaires <- paste(repgen, "Bases_intermediaires" , sep = "/")
 # Ici quelques paramètres généraux
 
 
+
 ################################################################################
 # =========== 01 = PACKAGES ET SCRIPTS DE FONCTIONS  ===========================
 ################################################################################
 source(paste(repo_prgm , "01_packages.R" , sep = "/"))
-
-
-
-
-
-
-
+source(paste(repo_prgm , "02_Importations_preparation.R" , sep = "/"))
 
 ################################################################################
-################### BROUILLON BENJAMIN ######################################### 
+# ================= 02 = PREPARATION DES DT  ===================================
 ################################################################################
+
 liste_cols_REI <- c("DEPARTEMENT",
                     "DIRECTION",
                     "COMMUNE",
@@ -55,22 +51,114 @@ liste_cols_REI <- c("DEPARTEMENT",
                     "FB.-.TSE./.BASE.NETTE",
                     "FB.-.TSE./.TAUX.NET")
 
-REI_21 <- data.table(read.xlsx(paste(repo_data, "REI_2021.xlsx", sep = "/")))
-SOUS_REI <- REI_21[, ..liste_cols_REI]
-save(SOUS_REI, file = paste(repo_bases_intermediaires, "REI_2021_SELECT.RData", sep = "/"))
+# Faire_dt_SOUS_REI(liste_cols_REI_loc) # A DECOMMENTER LA PREMIERE FOIS : Importe le REI, ne sélectionne que les colonnes de liste_cols_REI_loc et sauvegarde
+
+liste_cols_REI_loc <- c("DEPARTEMENT",
+                       "COMMUNE",
+                       "Numéro.national.du.groupement",
+                       "FB.-.COMMUNE./.TAUX.NET",
+                       "FB.-.GFP./.TAUX.APPLICABLE.SUR.LE.TERRITOIRE.DE.LA.COMMUNE",
+                       "FB.-.TSE./.TAUX.NET", 
+                       "Libellé.commune")
+
+dt_merged_REI <- Importer_et_merge_DT_REI(liste_cols_REI_loc)
+
+dt_merged_REI[is.na(FB_TSE_TAUX_NET)] # Bon on n'a pas les outre mer...
+100*nrow(dt_merged_REI[is.na(FB_TSE_TAUX_NET)])/nrow(dt_merged_REI) # Bon ça fait 0.21% des observations pas très grave sans doute
+
+################################################################################
+################### BROUILLON BENJAMIN ######################################### 
+################################################################################
 
 
 
+# # Puis Merge merged et REI
+# dt_merged_REI <- merge(dt_merged, SOUS_REI, by.x = c("ccodep", "ccocom"), by.y = c("DEPARTEMENT", "COMMUNE"), all.x = TRUE)
+# 
+# nrow(dt_merged)
+# nrow(dt_merged_REI)
+# 
+# table(dt_merged_REI[is.na(FB_TSE_TAUX_NET)]$ccodep) # Il trouve pas Paris Marseille Lyon...
+# 
+# 
+# SOUS_REI[DEPARTEMENT == 69 & Libellé.commune %like% "LYON"]
+# SOUS_REI[DEPARTEMENT == 13 & Libellé.commune %like% "MARSEILLE"]
+# SOUS_REI[DEPARTEMENT == 75 & Libellé.commune %like% "PARIS"]
+# 
+# table(carac_tf[ccodep== 69]$ccocom)
+# 
+# liste_num_Lyon <- 381:389
+# liste_num_Marseille <- 201:216
+# liste_num_Paris <- 101:120
+#   
+# carac_tf[, ccocom_REI := ccocom]
+# carac_tf[ccodep == "69" & ccocom_REI %in% liste_num_Lyon, ccocom_REI := "123"] # On met le code de la commune pour avoir le REU, et pas le code des arrondissement
+# carac_tf[ccodep == "13" & ccocom_REI %in% liste_num_Marseille, ccocom_REI := "055"] 
+# carac_tf[ccodep == "75" & ccocom_REI %in% liste_num_Paris, ccocom_REI := "056"]
+# 
+# # Merge TF et MEN
+# dt_merged <- merge(carac_tf, carac_men, by.x = "ident21", by.y = "ident")
+# 
+# # Puis Merge merged et REI
+# dt_merged_REI <- merge(dt_merged, SOUS_REI, by.x = c("ccodep", "ccocom_REI"), by.y = c("DEPARTEMENT", "COMMUNE"), all.x = TRUE)
+# 
+# nrow(dt_merged)
+# nrow(dt_merged_REI)
+# 
+# table(dt_merged_REI[is.na(FB_TSE_TAUX_NET)]$ccodep)
+# dt_merged_REI[is.na(FB_TSE_TAUX_NET)]
 
 
 
+# Lyon 1er Arrondissement (69381)
+# Lyon 2e Arrondissement (69382)
+# Lyon 3e Arrondissement (69383)
+# Lyon 4e Arrondissement (69384)
+# Lyon 5e Arrondissement (69385)
+# Lyon 6e Arrondissement (69386)
+# Lyon 7e Arrondissement (69387)
+# Lyon 8e Arrondissement (69388)
+# Lyon 9e Arrondissement (69389)
+
+# Marseille 1er Arrondissement (13201)
+# Marseille 2e Arrondissement (13202)
+# Marseille 3e Arrondissement (13203)
+# Marseille 4e Arrondissement (13204)
+# Marseille 5e Arrondissement (13205)
+# Marseille 6e Arrondissement (13206)
+# Marseille 7e Arrondissement (13207)
+# Marseille 8e Arrondissement (13208)
+# Marseille 9e Arrondissement (13209)
+# Marseille 10e Arrondissement (13210)
+# Marseille 11e Arrondissement (13211)
+# Marseille 12e Arrondissement (13212)
+# Marseille 13e Arrondissement (13213)
+# Marseille 14e Arrondissement (13214)
+# Marseille 15e Arrondissement (13215)
+# Marseille 16e Arrondissement (13216)
+
+# Paris 1er Arrondissement (75101)
+# Paris 2e Arrondissement (75102)
+# Paris 3e Arrondissement (75103)
+# Paris 4e Arrondissement (75104)
+# Paris 5e Arrondissement (75105)
+# Paris 6e Arrondissement (75106)
+# Paris 7e Arrondissement (75107)
+# Paris 8e Arrondissement (75108)
+# Paris 9e Arrondissement (75109)
+# Paris 10e Arrondissement (75110)
+# Paris 11e Arrondissement (75111)
+# Paris 12e Arrondissement (75112)
+# Paris 13e Arrondissement (75113)
+# Paris 14e Arrondissement (75114)
+# Paris 15e Arrondissement (75115)
+# Paris 16e Arrondissement (75116)
+# Paris 17e Arrondissement (75117)
+# Paris 18e Arrondissement (75118)
+# Paris 19e Arrondissement (75119)
+# Paris 20e Arrondissement (75120)
 
 
-
-
-
-carac_men <- data.table(readRDS(paste(repo_data, "carac_men.rds", sep = "/")))
-carac_tf <- data.table(readRDS(paste(repo_data, "carac_tf.rds", sep = "/")))
 
 # ident = identifiant ménage
 # aspa = Allocation solidarité aux personnes âgées
